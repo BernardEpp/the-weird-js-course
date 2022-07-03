118490bfef2d16ff203ce83df0c07cbb43cd3a3e
BernardEpp <bernard.epp@acommo.com>
2022-07-03 18:44:37 +0200

    Added bfs and dfs as graph search algorithms. Added path recreation in bfs.

--- graph-traversal/graph.js
+++ graph-traversal/graph.js
@@ -1,26 +1,33 @@
 
 // DATA 
-const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ');
+const cities = 'Frankfurt Mannheim Würzburg Stuttgart Nürnberg Kassel Karlsruhe Erfurt Augsburg München'.split(' ');
 
 const routes = [
-    ['PHX', 'LAX'],
-    ['PHX', 'JFK'],
-    ['JFK', 'OKC'],
-    ['JFK', 'HEL'],
-    ['JFK', 'LOS'],
-    ['MEX', 'LAX'],
-    ['MEX', 'BKK'],
-    ['MEX', 'LIM'],
-    ['MEX', 'EZE'],
-    ['LIM', 'BKK'],
+    ['Frankfurt', 'Mannheim'],
+    ['Frankfurt', 'Würzburg'],
+    ['Stuttgart', 'Mannheim'],
+    ['Frankfurt', 'Kassel'],
+    ['Karlsruhe', 'Mannheim'],
+    ['Stuttgart', 'Karlsruhe'],
+    ['Stuttgart', 'Augsburg'],
+    ['Augsburg', 'München'],
+    ['Kassel', 'Würzburg'],
+    ['Würzburg', 'Nürnberg'],
+    ['München', 'Nürnberg'],
+    ['München', 'Würzburg'],
+    ['Nürnberg', 'Erfurt'],
+    ['Frankfurt', 'Erfurt'],
 ];
 
 // The graph
 const adjacencyList = new Map();
 
+// The parent for each node.
+const parents = new Map();
+
 // Add Node 
-function addNode(airport) {
-    adjacencyList.set(airport, []);
+function addNode(city) {
+    adjacencyList.set(city, []);
 } 
 
 function addEdge(origin, destination) {
@@ -29,6 +36,82 @@ function addEdge(origin, destination) {
 }
 
 // Create the graph
-airports.forEach(addNode);
+cities.forEach(addNode);
 routes.forEach(route => addEdge(...route));
 
+// Breadth-first search (BFS)
+function bfs(start, target) {
+
+    const visited = new Set();
+
+    const queue = [start]; // first-in, first-out data structure of cities to look at
+  
+    while (queue.length > 0) {
+       
+        const city = queue.shift(); // look at the first element and remove it from queue
+        const connections = adjacencyList.get(city);
+
+        for (const connection of connections) {
+            
+            if (connection === target) {
+                console.log('found target');
+                parents.set(connection, city);
+                reconstructPath(start, target);
+            }
+
+            if (!visited.has(connection)) {
+                visited.add(connection);
+                queue.push(connection);
+                console.log(connection);
+                parents.set(connection, city);
+            }
+        }
+    }
+}
+
+// Depth-first search (DFS)
+function dfs(start, target, visited = new Set()) {
+    console.log(start);
+    visited.add(start);
+
+    const connections = adjacencyList.get(start);
+
+    for (const connection of connections) {
+        if (connection === target) {
+            console.log('found target');
+            return;
+        }
+    
+        if (!visited.has(connection)) {
+            dfs(connection, target, visited);
+        }
+    }
+}
+
+function reconstructPath(start, target){
+    console.log('reconstruct path');
+    const path = []
+
+    node = target;
+    while (node !== start) {
+        let parent = parents.get(node);
+        path.push(parent);
+        node = parent;
+    }
+    results.push(path);
+}
+
+
+// Setup search
+const start = 'Stuttgart';
+const end = 'Kassel';
+let results = [];
+
+
+// Run search / traverse
+bfs(start, end);
+// dfs(start, end);
+
+// Handle search result
+console.log('the result is: ')
+console.log(results);
